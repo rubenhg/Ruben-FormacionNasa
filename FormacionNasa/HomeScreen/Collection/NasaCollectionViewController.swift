ff6bfc38267806ccfa1c8da6322eea4fada6741b
Ruben Higueras Galan <ruben.higueragalan@plexus.es>
2020-10-15 16:10:20 +0200

    Loggin added.

--- FormacionNasa/HomeScreen/Collection/NasaCollectionViewController.swift
+++ FormacionNasa/HomeScreen/Collection/NasaCollectionViewController.swift
@@ -12,6 +12,7 @@ class NasaCollectionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .red
 
         // Do any additional setup after loading the view.
     }
--- FormacionNasa/LogginScreen/LogginViewController.swift
+++ FormacionNasa/LogginScreen/LogginViewController.swift
@@ -19,26 +19,27 @@ class LogginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setTexts()
-        setStyles()
+        
         // Do any additional setup after loading the view.
     }
     
     func setTexts(){
-        
-    }
-    
-    func setStyles(){
-        
+        emailTextField.placeholder = "Email Address"
+        passwordTextField.placeholder = "Password"
+        registerButton.setTitle("Sign Up", for: .normal)
+        logginButton.setTitle("Sign In", for: .normal)
     }
     
     @IBAction func registerAction(_ sender: Any) {
         if let email = emailTextField.text , let password = passwordTextField.text {
             Auth.auth().createUser(withEmail: email, password: password) { (completion, error) in
+                
                 if  error == nil{
-                            let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
-                            let nasaListViewController = storyBoard.instantiateViewController(withIdentifier: "nasaListViewController") as! NasaListViewController
-                            nasaListViewController.modalPresentationStyle = .fullScreen
-                            self.present(nasaListViewController, animated: true, completion: nil)
+                    let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+                    let tabBarController = storyBoard.instantiateViewController(withIdentifier: "tabBarController") as! UITabBarController
+                    tabBarController.modalPresentationStyle = .fullScreen
+                    self.present(tabBarController, animated: true, completion: nil)
+                    
                 }else{
                     let alert = UIAlertController(title: "Error", message: "Se ha producido un error en el registro de usuario. Compruebe que los datos introducidos sean correctos", preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "Cerrar", style: .default))
@@ -46,16 +47,26 @@ class LogginViewController: UIViewController {
                 }
             }
         }
-        
     }
     
     @IBAction func logginAction(_ sender: Any) {
-//        let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
-//        let nasaListViewController = storyBoard.instantiateViewController(withIdentifier: "nasaListViewController") as! NasaListViewController
-//        nasaListViewController.modalPresentationStyle = .fullScreen
-//        present(nasaListViewController, animated: true, completion: nil)
-    }
-    
+        if let email = emailTextField.text , let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { (completion, error) in
+                
+                if  error == nil{
+                   let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+                    let tabBarController = storyBoard.instantiateViewController(withIdentifier: "tabBarController") as! UITabBarController
+                    tabBarController.modalPresentationStyle = .fullScreen
+                    self.present(tabBarController, animated: true, completion: nil)
+                    
+                }else{
+                    let alert = UIAlertController(title: "Error", message: "Se ha producido un error en el inicio de sesion.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Cerrar", style: .default))
+                    self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
 
     /*
     // MARK: - Navigation
