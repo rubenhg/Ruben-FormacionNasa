c3ef136e6eb71cfd2cd0b3c2fade3b1151c19c84
Ruben Higueras Galan <ruben.higueragalan@plexus.es>
2020-10-15 14:04:06 +0200

    Register done.

--- FormacionNasa/LogginScreen/LogginViewController.swift
+++ FormacionNasa/LogginScreen/LogginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LogginViewController: UIViewController {
     
@@ -31,6 +32,21 @@ class LogginViewController: UIViewController {
     }
     
     @IBAction func registerAction(_ sender: Any) {
+        if let email = emailTextField.text , let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { (completion, error) in
+                if  error == nil{
+                            let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+                            let nasaListViewController = storyBoard.instantiateViewController(withIdentifier: "nasaListViewController") as! NasaListViewController
+                            nasaListViewController.modalPresentationStyle = .fullScreen
+                            self.present(nasaListViewController, animated: true, completion: nil)
+                }else{
+                    let alert = UIAlertController(title: "Error", message: "Se ha producido un error en el registro de usuario. Compruebe que los datos introducidos sean correctos", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "Cerrar", style: .default))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+        
     }
     
     @IBAction func logginAction(_ sender: Any) {
